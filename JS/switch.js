76c98d55365c00b351105c0c9c1c6791fde106c0
baratrahjaga <baratrahjaga@gmail.com>
2024-04-15 20:45:34 +0700

    mengembangkan logika di code

--- JS/switch.js
+++ JS/switch.js
@@ -1,14 +1,16 @@
-var i=prompt('masukkan angka...')
-var char='*'
-var sv=''
+var i= Number(prompt('masukkan angka...'))
+var char=''
 switch (i) {
-    case '0' :  
-    alert('please input Number')  
-    break;2
+    case 0 :  
+        alert('please input Number')  
+        break;2
     default:
         for(var n=1;n<=Number(i);n++){
-            sv=sv+char
-            console.log(sv)
+            console.log(char += "*")
+            if(char.length==i){
+                for (var b=i; b !==0;b--){
+                    console.log(char=char.slice(0, -1));}
+            }
         }
         break;
 }
