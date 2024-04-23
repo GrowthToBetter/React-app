448f4f6380f05b3b8a536f5f875c6954a5841bf8
baratrahjaga <baratrahjaga@gmail.com>
2024-04-23 14:01:40 +0700

    mendetailkan objgame yang kubuat

--- JS/objGame.js
+++ JS/objGame.js
@@ -33,7 +33,73 @@ function Angkot(angkot, sopir, biaya, track){
         }
         var kembalian= uang-this.biaya
         this.penghasilan +=this.biaya
-        return console.log(`kembalian anda ${kembalian}`);
+        return alert(`kembalian anda ${kembalian}`);
     }
 
+}
+
+var angkot1,
+    angkot2,
+    angkot3,
+    jenis,
+    nama,
+    ktp,
+    ask,
+    id,
+    bayar,
+    Run;
+
+angkot1=new Angkot([],'widy',10000,['palempang','yogyakarta'])
+angkot2= new Angkot([],'dawam', 15000,['malang','jakarta'])
+angkot3=new Angkot([],'didik',20000,['jakarta','bali'])
+Run=true
+while(Run){
+    nama= prompt('nama..')
+    ktp=prompt('nomer ktp...')
+    ask=prompt('naik/turun')
+    id={
+        'name':nama,
+        'id':ktp
+    }
+    switch (ask) {
+        case 'naik':
+            jenis=prompt('angkot 1-3')
+                switch (jenis) {
+                    case '1':
+                        angkot1.tambahPenumpang(id)
+                        console.log(`angkot 1 = ${angkot1}`)
+                        break;
+                    case '2':
+                        angkot2.tambahPenumpang(id)
+                        console.log(`angkot 2 = ${angkot2}`)
+                    case '3':
+                        angkot3.tambahPenumpang(id)
+                        console.log(`angkot 3 = ${angkot3}`)
+                    default:
+                        break;
+                }
+            break;
+        case 'turun':
+            jenis=prompt('angkot 1-3')
+            switch (jenis) {
+                case '1':
+                    bayar=Number(prompt('biaya 10000, masukkan uang anda'))
+                    angkot1.kurangPenumpang(id, bayar)
+                    console.log(`angkot 1 = ${angkot1}`)
+                    break;
+                case '2':
+                    bayar=Number(prompt('biaya 15000, masukkan uang anda'))
+                    angkot2.kurangPenumpang(id,bayar)
+                    console.log(`angkot 2 = ${angkot2}`)
+                case '3':
+                    bayar=Number(prompt('biaya 20000, masukkan uang anda'))
+                    angkot3.kurangPenumpang(id,bayar)
+                    console.log(`angkot 3 = ${angkot3}`)
+                default:
+                    break;
+            }
+        default:
+            break;
+    }
+    Run=confirm('lanjut? ')
 }
