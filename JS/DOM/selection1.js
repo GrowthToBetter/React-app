b80bcb55b08f804186ff424766e6eb6914715ad7
GrowthToBetter <baratrahjaga@gmail.com>
2024-05-24 05:53:21 +0700

    menambahkan metode selection in selection.js

--- JS/DOM/selection1.js
+++ JS/DOM/selection1.js
@@ -10,8 +10,18 @@ const elemenP=document.getElementsByTagName('p');
 for(let i=0;i<elemenP.length;i++){
     elemenP[i].style.border='5px solid black'
 }
-
-const item=document.getElementsByClassName('container')[0]
+// cari dengan class
+const item=document.getElementsByClassName('header')[0]
 item.style.backgroundColor='grey';
 
-
+// cari dengan elemen
+const p4=document.querySelector('#b p')
+p4.style.backgroundColor='#ff34';
+const ListItemId=document.querySelector('section#b ul li:nth-child(2)'
+)
+ListItemId.style.backgroundColor="#faba91"
+// cari semua elemen yang dipilih(tidak spesifik)
+const allElemenListItem=document.querySelectorAll('p')
+for(let i =0;i<allElemenListItem.length;i++){
+    allElemenListItem[i].style.fontSize='6rem'
+};
