a0273bf9118c56f9d82edda5dfd6e53a32d9140c
GrowthToBetter <baratrahjaga@gmail.com>
2024-05-24 04:29:11 +0700

    menambahkan selection sort di sort.js

--- JS/sort.js
+++ JS/sort.js
@@ -15,3 +15,30 @@ bubleSort(listItem)
 console.log(listItem)
 
 // selection sort
+console.log('=======selectionSort========')
+function selectionSort(arr){
+    let len=arr.length
+    for (let i=0;i<len-1;i++){
+        let smallestValue=i;
+        for(let j=i+1;j<len;j++){ 
+            if(arr[j]<arr[smallestValue]){
+                smallestValue=j;
+            }
+        }
+        if(smallestValue !==i){
+            let temp = arr[i];
+            arr[i] = arr[smallestValue];
+            arr[smallestValue] = temp;
+            console.log(arr)
+        }
+    }
+    return arr
+}
+
+let selectionSortList=[4,9,5,10,12,7,8]
+console.log(selectionSortList)
+selectionSort(selectionSortList)
+// quick sort
+
+
+
